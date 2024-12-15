e929e5c5efc4c49dfc25d894e922631b1185d35d
Jessen Forbush <for21011@byui.edu>
2024-12-15 10:49:50 -0700

    Target-Typed new variables.

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -62,3 +62,38 @@ StreamWriter file2 = File.CreateText("something2.txt");
 
 /* Value types have memory allocated on the stack automatically. */
 
+short age; // Allocates 2 bytes of memory on the stack to store a System.Int16 value.
+long population; // Allocates 8 bytes of memory on the stack to store a System.Int64 value.
+DateTime birthdate; // Allocates 8 bytes of memory on the stack to store a System.DateTime value.
+Point location; // Allocates 8 bytes of memory on the stack to store a System.Drawing.Point value.
+
+/* Reference types will only have memory allocated on the heap when new is used
+(but they automatically have some memory allocated on the stack to store information
+about themselves including the memory address of where they are on the heap.) */
+
+Person bob; /* Allocates memory in the stack that can point to a person object in the heap.
+Initially, bob will have the value null. */
+
+// Using new:
+
+age = 45; // Initialize this variable to 45 using a literal value.
+population = 68_000_000; // Initialize this variable to 68 million using a literal value.
+birthdate = new(1995, 2, 23); //Initialize this variable to February 23rd, 1995.
+location = new(10, 20); //Initialize the X and Y coordinates of this value type.
+bob = new(); /* Allocate memory on the heap to store a Person. Any state will have
+default values. Bob will no longer be null. */
+bob = new("Bob", "Smith", 45); /* Allocate memory on the heap to store a Person and initialize state. 
+Bob is no longer null. */
+
+
+// Using target-typed new to instantiate objects:
+
+XmlDocument xml13 = new();
+
+// This is especially useful with arrays and collections because they have multiple objects.
+List<Person> people = new() // Instead of: new List<Person>()
+{
+new() { FirstName = "Alice" }, // Instead of: new Person() { ... }
+new() { FirstName = "Bob" },
+new() { FirstName = "Charlie" }
+};
