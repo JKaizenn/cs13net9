328ffa8feb172d55f52a9330cf7d54838ab26b0e
Jessen Forbush <for21011@byui.edu>
2024-12-14 22:45:04 -0700

    Made a new variables project

--- Chapter02/Numbers/Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
+++ Chapter02/Numbers/Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a404c21a8c9f03b9a80ca7a49028060fa39c84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ed102ec4390a21708de06566fd77ab1cd3faea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
