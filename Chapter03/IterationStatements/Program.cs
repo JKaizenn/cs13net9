1490c713454f5c8f922122b1aaed5435c917ff68
Jessen Forbush <for21011@byui.edu>
2024-12-29 16:16:43 -0500

    Added do statement loop.

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 0;
+        while(x < 10)
+        {
+            WriteLine(x);
+            x++;
+        } 
+        // #region Looping with the do statement
+
+        // string? actualPassword = "Pa$$w0rd";
+        // string? password;
+        // int attempts = 0;
+        // int maxAttempts = 3;
+
+        // do
+        // {
+        //     Console.Write("Enter your password: ");
+        //     password = Console.ReadLine();
+        //     attempts++;
+
+        //     if (password == actualPassword)
+        //     {
+        //         Console.WriteLine("Correct!");
+        //         break;
+        //     }
+
+        //     if (attempts >= maxAttempts)
+        //     {
+        //         Console.WriteLine("Too many failed attempts. Access denied.");
+        //         break;
+        //     }
+        // }
+        // while (password != actualPassword);
+        // #endregion
+
         
     }
 }
