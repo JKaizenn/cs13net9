ef9046eeafeb1c24118ec097c5720ee06601cb09
Jessen Forbush <for21011@byui.edu>
2024-12-29 19:20:55 -0500

    Added pattern matching arrays.

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -92,7 +92,60 @@
                     WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
                 }
             }
+            WriteLine();
+            #endregion
 
+            #region Pattern Matching with Arrays
+
+            int[] sequentialNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] oneTwoNumbers = { 1, 2 };
+            int[] oneTwoTenNumbers = { 1, 2, 10 };
+            int[] oneTwoThreeTenNumbers = { 1, 2, 3, 10 };
+            int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+            int[] fibonacciNumbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+            int[] emptyNumbers = { }; // or use Array.Empty<int>()
+            int[] threeNumbers = { 9, 7, 5 };
+            int[] sixNumbers = { 9, 7, 5, 4, 2, 10 };
+
+            WriteLine($"{nameof(sequentialNumbers)}: {CheckSwitch(sequentialNumbers)}\n");
+
+            WriteLine($"{nameof(oneTwoNumbers)}: {CheckSwitch(oneTwoNumbers)}\n");
+
+            WriteLine($"{nameof(oneTwoTenNumbers)}: {CheckSwitch(oneTwoTenNumbers)}\n");
+
+            WriteLine($"{nameof(oneTwoThreeTenNumbers)}: {CheckSwitch(oneTwoThreeTenNumbers)}\n");
+
+            WriteLine($"{nameof(primeNumbers)}: {CheckSwitch(primeNumbers)}\n");
+
+            WriteLine($"{nameof(fibonacciNumbers)}: {CheckSwitch(fibonacciNumbers)}\n");
+
+            WriteLine($"{nameof(emptyNumbers)}: {CheckSwitch(emptyNumbers)}\n");
+
+            WriteLine($"{nameof(threeNumbers)}: {CheckSwitch(threeNumbers)}\n");
+
+            WriteLine($"{nameof(sixNumbers)}: {CheckSwitch(sixNumbers)}\n");
+
+            static string CheckSwitch(int[] values) => values switch
+            {
+                [] => "Empty array",
+
+                [1, 2, _, 10] => "Contains 1, 2, any single number, 10.",
+
+                [1, 2, .., 10] => "Contains 1, 2, any range including empty, 10.",
+
+                [1, 2] => "Contains 1 then 2.",
+
+                [int item1, int item2, int item3] =>
+                    $"Contains {item1} then {item2} then {item3}.",
+
+                [0, _] => "Starts with 0, then one other number.",
+
+                [0, ..] => "Starts with 0, then any range of numbers.",
+
+                [2, .. int[] others] => $"Starts with 2, then {others.Length} more numbers.",
+
+                _ => "Any items in any order.", // <-- Note the trailing comma for easier re-ordering.
+            };
             #endregion
         }
     }
