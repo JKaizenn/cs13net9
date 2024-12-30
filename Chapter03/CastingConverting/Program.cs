6cdb54a7f440645ec99e5335f4732e32d41f5126
Jessen Forbush <for21011@byui.edu>
2024-12-29 20:02:51 -0500

    Implicit and Explicit Casting Learnings.

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Implicitlyand Explicitly Casting
+
+        int a = 10;
+        double b = a; // An int can be safely cast into a double.
+        WriteLine($"a is {a}, b is {b}");
+
+        double c = 9.8;
+        int d = (int)c; // Compiler gives an error if you do not explicitly cast.
+        WriteLine($"c is {c}, d is {d}"); // d loses the .8 part
+
+        #endregion
+
     }
 }
