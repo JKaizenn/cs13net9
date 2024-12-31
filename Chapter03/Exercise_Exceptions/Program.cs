2f75cb676da1d0dd8e6e64c00dc621ada06da22e
Jessen Forbush <for21011@byui.edu>
2024-12-31 14:36:53 -0500

    Finished Simple Exceptions Exercise

--- Chapter03/Exercise_Exceptions/Program.cs
+++ Chapter03/Exercise_Exceptions/Program.cs
@@ -1,13 +1,27 @@
 ﻿namespace Exercise_Exceptions;
 
+using System;
+using System.ComponentModel;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter two numbers:");
-        string number1 = ReadLine(); // Properly invoke ReadLine with ()
-        string number2 = ReadLine(); // Properly invoke ReadLine with ()
+        Console.WriteLine("Please enter two numbers between 0-255:");
 
-        Console.WriteLine($"Your numbers are {number1} and {number2}");
+        // Converts the string to int
+        int input1 = int.Parse(ReadLine()); 
+        int input2 = int.Parse(ReadLine()); 
+
+        // Checks if the user input is between 0-255
+        if(input1 >= 0  && input2 >= 0 && input1 <= 255 && input2 <= 255)
+        {
+            WriteLine($"Number 1 divided by Number 2 = {input1 / input2}");
+        }
+        else 
+        {
+            WriteLine("Your number is not in the desired range, try again!");
+        }
+    
     }
 }
