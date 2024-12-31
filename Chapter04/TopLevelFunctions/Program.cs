697e994858643709cd9762f55bfcd5e457d19444
Jessen Forbush <for21011@byui.edu>
2024-12-31 15:41:09 -0500

    Whats my namespace example

--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -1,9 +1,16 @@
 ﻿namespace TopLevelFunctions;
+using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        WriteLine("* Top-Level functions example");
+        WhatsMyNamespace(); // Call the function
+    }
+
+    static void WhatsMyNamespace() // Define a local function (should be static)
+    {
+        WriteLine("Namespace of Program class: {0}", typeof(Program).Namespace ?? "null");
     }
 }
