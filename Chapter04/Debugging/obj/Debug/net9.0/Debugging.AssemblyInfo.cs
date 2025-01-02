54a1bfb928bbc563dd8b5945a23672be4e9f512d
Jessen Forbush <for21011@byui.edu>
2025-01-02 17:04:28 -0500

    Fixing errors with debugging

--- Chapter04/Debugging/obj/Debug/net9.0/Debugging.AssemblyInfo.cs
+++ Chapter04/Debugging/obj/Debug/net9.0/Debugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Debugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bc2cdac9e7a79ce141e6ffc1c44a06868abafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77788b91557b3113e5fb1c2aba295e79df8f6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Debugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Debugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
