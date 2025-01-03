bf0278c913dc1941ff631e954bac8a66ed71affb
Jessen Forbush <for21011@byui.edu>
2025-01-03 17:01:40 -0500

    Add project reference for CallStackExceptionHandlingLib in CallStackExceptionHandling project

--- Chapter04/CallStackExceptionHandling/obj/Debug/net9.0/CallStackExceptionHandling.AssemblyInfo.cs
+++ Chapter04/CallStackExceptionHandling/obj/Debug/net9.0/CallStackExceptionHandling.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CallStackExceptionHandling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a37f8cd31afa8a28a15628187420f35b1437e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76865d03b9f6ab97140502d91b084cdf1dcac582")]
 [assembly: System.Reflection.AssemblyProductAttribute("CallStackExceptionHandling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CallStackExceptionHandling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Chapter04/CallStackExceptionHandlingLib/obj/Debug/net9.0/CallStackExceptionHandlingLib.AssemblyInfo.cs
+++ Chapter04/CallStackExceptionHandlingLib/obj/Debug/net9.0/CallStackExceptionHandlingLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CallStackExceptionHandlingLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691c49538e7b76dbc7e42c11cfaa483f74e3000e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76865d03b9f6ab97140502d91b084cdf1dcac582")]
 [assembly: System.Reflection.AssemblyProductAttribute("CallStackExceptionHandlingLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CallStackExceptionHandlingLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
