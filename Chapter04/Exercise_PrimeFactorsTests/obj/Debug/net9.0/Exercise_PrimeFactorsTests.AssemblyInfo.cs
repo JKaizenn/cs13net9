f7f66fefc511d6267b98c2a4dc5b06f90ab0bbd2
Jessen Forbush <for21011@byui.edu>
2025-01-07 19:27:10 -0700

    Update project files and dependencies for Exercise_PrimeFactorsApp and Tests

--- Chapter04/Exercise_PrimeFactorsTests/obj/Debug/net9.0/Exercise_PrimeFactorsTests.AssemblyInfo.cs
+++ Chapter04/Exercise_PrimeFactorsTests/obj/Debug/net9.0/Exercise_PrimeFactorsTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise_PrimeFactorsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a171b2617de937ddf6708ccfa876b50c3227c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178a7c8c8fe1d49b9728f035d1e8ca146c916640")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise_PrimeFactorsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise_PrimeFactorsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
